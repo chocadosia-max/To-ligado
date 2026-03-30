dbddcde5b3bdcbe8a138f988b8c80495cec8698b
chocadosia-max <chocados.ia@gmail.com>
2026-03-30 02:29:41 -0300

    fix-wa-version-and-ua

--- src/index.js
+++ src/index.js
@@ -78,9 +78,14 @@ const client = new Client({
   authStrategy: new LocalAuth({
     dataPath: process.env.SESSION_PATH ?? './sessions',
   }),
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  },
   puppeteer: {
     headless: true,
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH ?? undefined,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
